72751df7a1af0ab51788f3bda4769f47a39180f8
saivarshinii <41728683+saivarshinii@users.noreply.github.com>
2018-07-26 16:07:04 +0530

    Create Sai1.java

---
+++ varshi/Sai1.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Sai1
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	   int a=3;
+	   if(a<0)
+	   { 
+	   	System.out.println("negative");
+	   }else if(a==0)
+	   {
+	   	System.out.println("zero");
+	   }
+	   System.out.println("positive");
+	
+	   }
+	}

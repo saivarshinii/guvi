3679e67279d715e12b2874af47bddd375ebb5e16
saivarshinii <41728683+saivarshinii@users.noreply.github.com>
2018-07-26 16:29:49 +0530

    Create saii.java

---
+++ saii.java
@@ -0,0 +1,26 @@
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Saii
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	   Scanner sc=new Scanner(System.in);
+	   int a=sc.nextInt();
+	   if(a>0)
+	   { 
+	   	System.out.println("Positive");
+	   }else if(a<0)
+	   {
+	   	System.out.println("Negative");
+	   	
+	   }else{
+	   	System.out.println("zero");
+	   }
+	   
+	}
+}

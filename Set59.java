2c6df65deaf0927f171b3a1bafb6c7883836097e
saivarshinii <41728683+saivarshinii@users.noreply.github.com>
2018-08-17 10:37:58 +0530

    Create Set59.java

---
+++ Set59.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Set59
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int p=s.nextInt();
+		System.out.print(n+p);
+	}
+}
